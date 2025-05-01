82634fec222cb0f61cc57021ba3aa351e24059b5
Shadab Faisal <shadabfaisal91@gmail.com>
2025-05-02 00:22:14 +0530

    Added send email with attachment API

--- Backend/spring-mailer/src/main/java/com/springboot/spring_mailer/controllers/EmailController.java
+++ Backend/spring-mailer/src/main/java/com/springboot/spring_mailer/controllers/EmailController.java
@@ -1,12 +1,14 @@
 package com.springboot.spring_mailer.controllers;
 
+import com.springboot.spring_mailer.entities.CustomResponse;
 import com.springboot.spring_mailer.entities.EmailRequest;
 import com.springboot.spring_mailer.services.EmailService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("api/v1/email")
@@ -19,9 +21,23 @@ public class EmailController {
     }
 
     @PostMapping("/send")
-    public ResponseEntity<?> sendEmail(@RequestBody EmailRequest request){
+    public ResponseEntity<CustomResponse> sendEmail(@RequestBody EmailRequest request){
         emailService.sendEmailWithHtml(request.getTo(), request.getSubject(), request.getMessage());
-        return ResponseEntity.ok();
+        return ResponseEntity.ok(
+                CustomResponse.builder().message(
+                        "Email sent successfully!!!"
+                ).httpStatus(HttpStatus.OK).success(true).build()
+        );
+    }
+
+    @PostMapping("/send-with-file")
+    public ResponseEntity<CustomResponse> sendEmailWithAttachment(@RequestPart EmailRequest request, @RequestPart MultipartFile file) throws IOException {
+            emailService.sendEmailWithFile(request.getTo(), request.getSubject(), request.getMessage(), file.getInputStream());
+            return ResponseEntity.ok(
+                CustomResponse.builder().message(
+                        "Email sent successfully!!!"
+                ).httpStatus(HttpStatus.OK).success(true).build()
+        );
     }
 
 }
